38cba75006b4363dc9879a95f33c64b482a5e258
Bekidemissie <bekidemissie16@gmail.com>
2023-09-07 14:02:45 -0400

    fixing the dispaly to come main page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,19 +6,20 @@
 
 
 $(document).ready(function () {
-    const escape = function (str) {
-      let div = document.createElement("div");
-      div.appendChild(document.createTextNode(str));
-      return div.innerHTML;
-    };
-  
-    //Slide the new tweet box
-  
-    $("#arrow").click(function () {
-      $(".new-tweet").slideDown();
-    });
-    const createTweetElement = function (tweet) {
-      let $tweet = `
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+    loadTweets();
+  };
+
+  //Slide the new tweet box
+
+  $("#arrow").click(function () {
+    $(".new-tweet").slideDown();
+  });
+  const createTweetElement = function (tweet) {
+    let $tweet = `
       <section class='tweet'>
   
     <article>
@@ -32,12 +33,12 @@ $(document).ready(function () {
       <main>
               <p>
                 ${escape(
-                  tweet.content.text.length > 60
-                    ? tweet.content.text.slice(0, 60) +
-                        "\n" +
-                        tweet.content.text.slice(60, tweet.content.text.length)
-                    : tweet.content.text
-                )}
+      tweet.content.text.length > 60
+        ? tweet.content.text.slice(0, 60) +
+        "\n" +
+        tweet.content.text.slice(60, tweet.content.text.length)
+        : tweet.content.text
+    )}
               </p>
             </main>
             <footer id='tweet-foot'>
@@ -52,61 +53,75 @@ $(document).ready(function () {
             </footer>
           </article>
           </section>`;
-      return $tweet;
-    };
+    return $tweet;
+  };
+
   
-    const renderTweets = function (arr) {
-      const $container = $(".container");
-      $.each(arr, (key) => {
-        $container.prepend(createTweetElement(arr[key]));
-      });
-  
-      return $container;
-    };
-  
-    //Submits form
-    const $form = $(".textarea");
-  
-    $form.submit(function (event) {
-      event.preventDefault();
-      //cleans up any leftover error messages
-      $("#empty").slideUp();
-      $("#long-error").slideUp();
-      $(".new-tweet").slideUp();
-      //form validation checks
-      const newTweetData = event.target[0].value;
-      if (!newTweetData) {
-        $("#empty").slideDown();
-        $(".new-tweet").slideDown();
-        return;
-      }
-  
-      if (newTweetData.length > 140) {
-        $("#long-error").slideDown();
-        $(".new-tweet").slideDown();
-        return;
-      }
-  
-      $.ajax({
-        method: "POST",
-        url: "http://localhost:8080/tweets",
-        data: $(this).serialize()
-      }).then(function () {
-        loadTweets();
-      });
+  const renderTweets = function (arr) {
+    const $container = $("#tweets-container"); // Select the container
+    $container.empty(); // Clear existing tweets
+
+    $.each(arr, (key) => {
+      $container.prepend(createTweetElement(arr[key])); // Append each tweet
     });
-  
-    //fetching tweets from /tweets page
-  
-    const loadTweets = function () {
-      $.ajax({
-        method: "GET",
-        url: "http://localhost:8080/tweets",
-      }).then(function (tweet) {
-        renderTweets(tweet);
-        //resets the form
-        document.querySelector(".textarea").reset();
-      });
-    };
-    loadTweets();
+
+    return $container;
+  };
+
+  //Submits form
+  const $form = $(".textarea");
+
+  $form.submit(function (event) {
+    event.preventDefault();
+    //cleans up any leftover error messages
+    $("#empty").slideUp();
+    $("#long-error").slideUp();
+
+    //form validation checks
+    const newTweetData = event.target[0].value.trim();
+    if (!newTweetData) {
+      $("#empty").slideDown();
+      $(".new-tweet").slideDown();  // Show the .new.-tweet container
+      return;
+    }
+
+    if (newTweetData.length > 140) {
+      $("#long-error").slideDown();
+      $(".new-tweet").slideDown();  // Show the .new-tweet container
+      return;
+    }
+
+    $.ajax({
+      method: "POST",
+      url: "http://localhost:8080/tweets",
+      data: $(this).serialize()
+    }).then(function () {
+      // Reset the textarea and tweet counter
+      event.target[0].value = '';
+      $(event.target).find('.counter').text('140');
+
+      // Check if .new-tweet is not already visible
+      if (!$(".new-tweet").is(":visible")) {
+        // Show the .new-tweet container
+        $(".new-tweet").slideDown();
+      }
+
+      // Load and display tweets again
+      loadTweets();
+    });
   });
+
+  //fetching tweets from /tweets page
+  
+  const loadTweets = function () {
+    $.ajax({
+      method: "GET",
+      url: "http://localhost:8080/tweets",
+    }).then(function (tweet) {
+      renderTweets(tweet);
+      //resets the form
+
+    });
+  };
+  loadTweets();
+});
